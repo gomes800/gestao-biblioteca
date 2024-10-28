faf2a49010b53b89e5ffff92f7b090cadefe7f9e
barox <joaogomesleite800@gmail.com>
2024-10-28 09:54:53 -0300

    Conexão com banco de dados postgree

---
+++ src/main/java/com/gomes/biblioteca/entities/Usuario.java
@@ -0,0 +1,71 @@
+package com.gomes.biblioteca.entities;
+
+import java.util.Objects;
+
+public class Usuario {
+
+    private long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String tipo;
+
+    public Usuario(long id, String nome, String email, String senha, String tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id == usuario.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+}
---
+++ src/main/java/com/gomes/biblioteca/entities/test.java
@@ -0,0 +1,25 @@
+package com.gomes.biblioteca.entities;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class test implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            if (dataSource.getConnection() != null) {
+                System.out.println("Conexão com o banco de dados estabelecida com sucesso!");
+            }
+        } catch (Exception e) {
+            System.err.println("Falha ao conectar ao banco de dados: " + e.getMessage());
+        }
+    }
+}
